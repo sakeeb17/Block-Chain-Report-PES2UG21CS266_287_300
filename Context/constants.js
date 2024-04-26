3bd68612e27c9ad9c2f9d5121f20989c1b95b983
MD <k68sami@gmail.com>
2024-04-27 00:13:12 +0530

    first commit

---
+++ Context/constants.js
@@ -0,0 +1,6 @@
+import nftMarketplace from "./NFTMarketplace.json";
+
+//NFT MARKETPLACE
+export const NFTMarketplaceAddress =
+  "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
+export const NFTMarketplaceABI = nftMarketplace.abi;
